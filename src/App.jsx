29daf02a634feb82318ce14da483d72909890020
Georgyo luka <georgyoluka.maralha@gmail.com>
2025-07-14 22:19:31 -0300

    modificaçoes jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
 import { useState } from "react";
+import AddTasks from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
 function App() {
-  const [mensagemOculta, setMensagemOculta] = useState("Olá mundo!")
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      description:
+        "Estudar programação para me tornar um desenvolvedor completo",
+      isCompletede: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Ingles",
+      description: "Estudar ingles para me tornar um Dev na gringa",
+      isCompletede: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Java",
+      description: "Estudar Java para complementar meu conhecimentos",
+      isCompletede: false,
+    },
+  ]);
 
-
+  function onTaskClick(taskId) {
+    const newTask = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTask);
+  }
 
   return (
-    <div>
-      <h1>{mensagemOculta}</h1>
-      <button onClick={() => {
-       setMensagemOculta("Olá, essa é uma mensagem oculta");
-      }}
-      >Mudar mensagem</button>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center ">
+          Gerenciador de tarefas
+        </h1>
+        <AddTasks />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
     </div>
-  )
-
+  );
 }
 
 export default App;
